8a639f596c3884405e95c9de3ef288324e068f81
khuskivadze <tamarkhuskivadze94@gmail.com>
2023-12-21 21:29:29 +0400

    add

--- app/Components/MainPage/index.js
+++ app/Components/MainPage/index.js
@@ -14,3 +14,28 @@ function removeMenu () {
     navMenu.classList.remove('showMenu')
 }
 closeMenuBtn.addEventListener('click', removeMenu)
+
+
+
+//Open login Section
+const openLoginSection = document.getElementById("home_btn");
+ 
+
+function openLogFunq () {
+    logInSection.classList.add('log-inShow')
+    logInSection.style.transition  = '0.8s ease'
+}
+openLoginSection.addEventListener('click', openLogFunq)
+
+
+//close login section
+
+const closeLogBtn = document.getElementById("close_log");
+const logInSection = document.getElementById("log-in");
+
+
+function closeLogFunq () {
+    logInSection.classList.remove('log-inShow')
+    console.log("close")
+}
+closeLogBtn.addEventListener('click', closeLogFunq)
